0dcace34975e9583959ea58a326c6f3c39006959
Manuel Carrasco <mcarrasco@dc.uba.ar>
2019-08-30 17:47:25 -0300

    Added example for generics

---
+++ tac2cil/Tests/Resources/Generics.cs
@@ -0,0 +1,39 @@
+﻿using System;
+namespace Test
+{
+    public class Program
+    {
+        public static int Test()
+        {
+            A<int> a = new A<int>(0);
+            a.Set(1);
+            return a.Get();
+        }
+
+        public static int Test1()
+        {
+            A<int> a = new A<int>(0);
+            a.i = 5;
+            return a.Get();
+        }
+    }
+    public class A<T>
+    {
+        public T i;
+
+        public A(T _i)
+        {
+            i = _i;
+        }
+
+        public void Set(T _i)
+        {
+            i = _i;
+        }
+
+        public T Get()
+        {
+            return i;
+        }
+    }
+}
