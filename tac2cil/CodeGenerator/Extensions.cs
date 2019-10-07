c9721f6db257245a4839840ac5d9de0e02520a29
Manuel Carrasco <mcarrasco@dc.uba.ar>
2019-10-06 23:26:45 -0300

    Missing file.

---
+++ tac2cil/CodeGenerator/Extensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeGenerator
+{
+    public static class Extensions
+    {
+        // todo: once analysis-net works correctly regarding generic parameter count and arguments, we could use analysis-net's extension
+        public static string MetadataName(this Model.Types.IBasicType basicType)
+        {
+            if (basicType.GenericParameterCount == 0)
+                return basicType.Name;
+
+            var arguments = string.Empty;
+
+            if (basicType.GenericArguments.Count > 0)
+            {
+                arguments = string.Join(", ", basicType.GenericArguments);
+                arguments = string.Format("<{0}>", arguments);
+            }
+
+            return string.Format("{0}`{1}{2}", basicType.Name, basicType.GenericParameterCount, arguments);
+        }
+    }
+}
