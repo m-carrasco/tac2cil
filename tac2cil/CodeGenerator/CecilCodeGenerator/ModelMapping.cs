2acbecf4f1b22afa0825b54da7f23e7f91cf1173
Manuel Carrasco <mcarrasco@dc.uba.ar>
2019-10-12 17:56:27 -0300

    Missing file

---
+++ tac2cil/CodeGenerator/CecilCodeGenerator/ModelMapping.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using AnalysisNet = Model;
+using Cecil = Mono.Cecil;
+namespace CodeGenerator.CecilCodeGenerator
+{
+    // the class holds a mapping between analysis-net and cecil code model objects
+    class ModelMapping
+    {
+        public ModelMapping()
+        {
+            FieldsMap = new Dictionary<AnalysisNet.Types.IFieldReference, Cecil.FieldReference>();
+            TypesMap = new Dictionary<AnalysisNet.Types.IType, Cecil.TypeReference>();
+            MethodsMap = new Dictionary<AnalysisNet.Types.IMethodReference, Cecil.MethodReference>();
+            AssembliesMap = new Dictionary<AnalysisNet.Assembly, Cecil.AssemblyDefinition>();
+        }
+
+        public IDictionary<AnalysisNet.Assembly, Cecil.AssemblyDefinition> AssembliesMap { get; }
+        public IDictionary<AnalysisNet.Types.IMethodReference, Cecil.MethodReference> MethodsMap { get; }
+        public IDictionary<AnalysisNet.Types.IType, Cecil.TypeReference> TypesMap { get; }
+        public IDictionary<AnalysisNet.Types.IFieldReference, Cecil.FieldReference> FieldsMap { get; }
+
+        public AnalysisNet.Assembly GetAnalysisNetAssembly(AnalysisNet.IAssemblyReference assemblyReference)
+        {
+            var assemblyQuery = AssembliesMap.Keys.Where(a => a.MatchReference(assemblyReference));
+
+            if (assemblyQuery.Count() > 1)
+                throw new Exception("at most one assembly should match");
+
+            AnalysisNet.Assembly assembly = assemblyQuery.FirstOrDefault();
+
+            return assembly;
+        }
+    }
+}
